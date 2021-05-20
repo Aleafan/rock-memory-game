fce53187121f62f175c942ec79f1b3a502632f00
Aleafan <ae.afanasov@yandex.com>
2021-05-20 17:48:43 +0300

    Create Header.js with header.css

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../styles/header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>ROCK MEMORY GAME</h1>
+            <p>Get points by clicking on an image but don't click on any more than once!</p>
+        </header>
+    );
+}
+
+export default Header

6ecfa6703eed88c964592ae85977d3a002d44c8e
Aleafan <ae.afanasov@yandex.com>
2021-05-22 14:39:05 +0300

    Create BestScore.js

---
+++ src/components/BestScore.js
@@ -0,0 +1,9 @@
+const BestScore = (props) => {
+    return (
+        <div>
+            <p><strong>Best Score: 10</strong></p>
+        </div>
+    );
+}
+
+export default BestScore
--- src/components/Playfield.js
+++ src/components/Playfield.js
@@ -2,12 +2,14 @@ import { data } from '../data.js';
 import '../styles/playfield.css';
 
 import Score from './Score';
+import BestScore from './BestScore';
 import Card from './Card';
 
 const Playfield = () => {
 	return (
 		<main>
 			<Score />
+			<BestScore />
 			<div id='cards-grid'>
 				{data.map(card => 
 					<Card key={card.name} card={card} />

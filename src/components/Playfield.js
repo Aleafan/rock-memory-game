de3b6d8961fc6d59d7e71dd645e873cc2591e6e9
Aleafan <ae.afanasov@yandex.com>
2021-11-15 17:45:14 +0300

    Remove useEffect import

--- src/components/Playfield.js
+++ src/components/Playfield.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import { data } from '../data.js';
 import '../styles/playfield.css';
 
